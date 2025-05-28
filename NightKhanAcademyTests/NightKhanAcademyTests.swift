2a29c2523f1be1233c998f5c175c3639c2da328b
zhrispineda <zhris@zhr.is>
2025-05-28 19:06:20 -0400

    Initial Commit

---
+++ NightKhanAcademyTests/NightKhanAcademyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NightKhanAcademyTests.swift
+//  NightKhanAcademyTests
+//
+//  Created by Chris on 5/28/25.
+//
+
+import Testing
+@testable import NightKhanAcademy
+
+struct NightKhanAcademyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
