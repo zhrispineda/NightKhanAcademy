7d6e90628c5bac4173738798a136136cd632f91a
zhrispineda <zhris@zhr.is>
2025-05-30 11:03:09 -0400

    Update content and README

--- NightKhanAcademy Extension/Resources/content.js
+++ NightKhanAcademy Extension/Resources/content.js
@@ -13,12 +13,21 @@ if (darkModeEnabled) {
 function injectDarkMode() {
     style = document.createElement('style');
     style.textContent = `
-        /* Base backgrounds and elements */
-        html, main, ._1v7bqtc2, #header-dropdown, #top-header-container, ._1dbhd502 {
+        /* ELEMENTS 
+        _fryalgi: Course challenge footer
+        _6vli2nb: Course mastery highlight box
+        */
+        html, main, ._1v7bqtc2, #header-dropdown, #top-header-container, ._1dbhd502, ._fryalgi, ._6vli2nb {
             background-color: #121212 !important;
         }
       
-        .stp-animated-banner, ._158ir6wt, ._vmquk6f, #username-picker-container, .modal-footer, ._fss88w4 {
+        /* ELEMENTS 
+        _xmj6ftz: Sidebar course header
+        _1g9xbd8f: Sidebar course units
+        _10q4fwvt: Course challenge box
+        _1yg7xuga: Exercise hover box
+        */
+        .stp-animated-banner, ._158ir6wt, ._vmquk6f, #username-picker-container, .modal-footer, ._fss88w4, ._xmj6ftz, ._1g9xbd8f, ._10q4fwvt, ._1yg7xuga, ._pxfwtyj, ._1tvlo9eq {
             background-color: #191919 !important;
         }
       
@@ -26,21 +35,39 @@ function injectDarkMode() {
             background-color: #1F1F1F !important;
         }
       
+        /* Unit section border */
         ._c53vsu1 {
             border: solid 1px rgba(200,200,200,0.50) !important;
         }
       
-        /* Texts to update for dark mode */
-        h2, h3, h4, b, ._1h62wb2e, ._1ltrm5gv, ._1m7imn5w, ._9pl6rtj, ._1irdsj0v, ._167zy1df, ._1bkn1zo, ._7mb6uf5, ._136dqw5w, ._1h09evv, .badge-count, ._1ell7dku, ._1hl2debl, ._8ry3zep, ._1q0d09t, ._dwmetq {
-            color: #FFFFFF !important;
+        /* TEXT
+        _1rhtxmd7: Course challenge title
+        _19kv87av: Course challenge book icon
+        _vn21y15: Course chellenge description
+        _5nw48vg: Sidebar unit title
+        _yutmpt: Course mastery unit title
+        _pqblyia: Course mastery legend text
+        _w6ztkyn: Course page title
+        _1izjhmh9: Course challenge footer title
+        _1fzwn6xn: Course challenge footer description
+        _1bm1zjqm: Up next text
+        _1tmst71o: Up next sparkle icon
+        */
+        h2, h3, h4, b, ._1h62wb2e, ._1ltrm5gv, ._1m7imn5w, ._9pl6rtj, ._1irdsj0v, ._167zy1df, ._1bkn1zo, ._7mb6uf5, ._136dqw5w, ._1h09evv, .badge-count, ._1ell7dku, ._1hl2debl, ._8ry3zep, ._1q0d09t, ._dwmetq, ._1rhtxmd7, ._19kv87av, ._vn21y15, ._5nw48vg, ._yutmpt, ._pqblyia, ._w6ztkyn, ._1izjhmh9, ._1fzwn6xn, ._1bm1zjqm, ._1tmst71o {
+            color: #ffffff !important;
         }
       
-        /* Selected text */
-        ._eheaiue, ._1k43t9ye, ._x1h08ae, ._1ejvexq8, ._1nlxlc8a, .empty-field {
+        /* Selected/hyperlink text */
+        ._eheaiue, ._1k43t9ye, ._x1h08ae, ._1ejvexq8, ._1nlxlc8a, .empty-field, ._i7xxeac {
             color: #1865F2 !important;
         }
       
-        /* Streak explanation bubble background, level explanation bubble background */
+        /* Sidebar bottom gradient */
+        ._1t2avldr {
+            background: linear-gradient(to bottom, transparent 0%, black 80%) !important;
+        }
+      
+        /* Streak explanation bubble background */
         ._xv24cwj {
             background-color: #121212 !important;
         }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # NightKhanAcademy
+A macOS Safari app extension that allows for toggling dark mode on Khan Academy.
 
+## Information
+- This project injects custom CSS into Khan Academy to give it a dark mode.
+- The button is displayed under the Settings button within the name dropdown menu when logged in.
+- This project currently targets macOS only at this time.
+
+## Usage
+Open the project in Xcode, build, run, and enable the extension in Safari.
+
+> [!IMPORTANT]  
+> You may need to enable `Allow unsigned extensions` under the Developer tab in Safari settings. If you do not see this option, you may need to enable `Show features for web developers` under Advanced in Safari settings.
